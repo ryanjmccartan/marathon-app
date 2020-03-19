535930fe16266cc975fbc5339f9d58edcb7cf015
Ryan McCartan <ryanjmccartan@gmail.com>
2020-03-19 14:49:04 -0500

    data is displaying

--- src/components/AddMiles.js
+++ src/components/AddMiles.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonItem, IonInput, IonList, IonText } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonItem, IonInput, IonList, IonText, IonCard } from '@ionic/react';
 import firebase from '../firebaseconfig';
 
 class AddMiles extends Component {
@@ -11,26 +11,43 @@ class AddMiles extends Component {
         this.state = {
             miles: 0,
             time: 0,
-            date: new Date(),
-            mile: []
-        }
+            date: new Date()        }
     }
 
-    componentDidMount() {
+//   componentDidMount() {
+//     this.getData();
+//   }
 
-        this.rootRef.on('value', data => {
-            data.forEach(milesnap => {
-                let milekey = milesnap.key;
-                let miledata = milesnap.val();
-                this.state.mile.push({
-                    miles: milesnap.val().miles,
-                    time: milesnap.val().time,
-                    date: milesnap.val().date
-                })
-                console.log(milekey, miledata);
-                console.log(this.state.mile);
-            })
+//   getData = () => {
+//     this.setState({
+//         data: []
+//     })
+//     this.rootRef.on('value', data => {
+//         data.forEach(milesnap => {
+//             let milekey = milesnap.key;
+//             let miledata = milesnap.val();
+//             this.state.data.push({
+//                 id: milekey,
+//                 miles: milesnap.val().miles,
+//                 time: milesnap.val().time,
+//                 date: milesnap.val().date
+//             })
+//         console.log(milekey, miledata);
+//         console.log('this is tracker data', this.state.data)
+//         })
+//     })
+// }
+
+addMiles = () => {
+    this.rootRef.push(this.state);
+    this.setState({
+            miles: 0,
+            time: 0,
+            date: new Date()
     })
+    // this.getData();
+    console.log('miles added', this.state);
+}
         // this.rootRef.on('value', data => {
         //     data.forEach(milesnap => {
         //         let milekey = milesnap.key;
@@ -60,11 +77,9 @@ class AddMiles extends Component {
         //     })
         // })
 
-    }
+ 
 
-    addMiles = () => {
-        this.rootRef.push(this.state);
-    }
+   
 
     handleChange = (e, input) => {
         this.setState({
@@ -93,26 +108,13 @@ class AddMiles extends Component {
     render() {
         return(
             <>
-                <IonItem>
-                {this.state.mile.map(mile => {
-                    return <IonText key={mile.key}><h1>{mile.miles}
-                    {mile.time}
-                    {mile.date}</h1>
-                    </IonText>
-                
-                })}
-                </IonItem>
-                {/* <IonHeader>{this.state.miles}</IonHeader>
-                <IonHeader>{this.state.time}</IonHeader> */}
-                {/* <IonList>{this.rootRef}</IonList> */}
-                {/* <IonHeader>{this.state.mile}</IonHeader> */}
-           
                 <IonInput onIonChange={(e) => this.handleChange(e, 'miles')} type="number" placeholder="miles ran"/>
                 <IonInput onIonChange={(e) => this.handleChange(e, 'time')} type="number" placeholder="time taken"/>
                 <IonInput onIonChange={(e) => this.handleChange(e, 'date')} type="date" placeholder="date"/>
                 <IonButton onClick={this.addMiles} color="secondary">Add miles</IonButton>
-                <IonButton onClick={this.checkData}>Check data</IonButton>
-                </>
+                {/* <IonButton onClick={this.checkData}>Check data</IonButton> */}
+           </>
+                
         )
     }
 }
--- src/pages/Tab1.js
+++ src/pages/Tab1.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonText } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import AddMiles from '../components/AddMiles';
 import './Tab1.css';
@@ -7,12 +7,44 @@ import firebase from '../firebaseconfig';
 
 class Tab1 extends Component {
 
+  constructor(props) {
+    super(props);
+    this.rootRef = firebase.database().ref().child('tracker');
+    this.state = {
+      key: '',
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    this.rootRef.on('value', data => {
+        data.forEach(milesnap => {
+            let milekey = milesnap.key;
+            let miledata = milesnap.val();
+            this.setState({
+              key: milesnap.key,
+              data: [...this.state.data, miledata]
+                // id: milekey,
+                // miles: milesnap.val().miles,
+                // time: milesnap.val().time,
+                // date: milesnap.val().date
+            })
+        console.log(milekey, miledata);
+        console.log('this is tracker data', this.state)
+        })
+    })
+}
+
   render() {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Journal</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
@@ -21,6 +53,26 @@ class Tab1 extends Component {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonText>{this.state.data.map(data => {
+          return <h1>Miles:
+            {data.miles}
+            Time:
+          {data.time}
+          Date: 
+          {data.date}</h1>
+        })}</IonText>
+        {/* <IonCard>
+                    <IonText>
+                {this.state.data.map(mile => {
+                    return <h1 key={mile.id}>{mile.miles}
+                    {mile.time}
+                    {mile.date}</h1>
+                    
+                
+                })}
+                </IonText>
+                </IonCard> */}
+              
         {/* <ExploreContainer name="Tab 1 page" /> */}
         <AddMiles/>
       </IonContent>
